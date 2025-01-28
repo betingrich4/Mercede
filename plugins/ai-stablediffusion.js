cc5af01348d2c1398ca002ce18d7082ef7e73f98
betingrich4 <mariseltru@gmail.com>
2025-01-28 20:34:22 +0300

    Add files via upload

---
+++ plugins/ai-stablediffusion.js
@@ -0,0 +1,52 @@
+
+const config = require('../config');
+const { cmd, commands } = require('../command');
+const { fetchJson } = require('../lib/functions');
+
+cmd({
+pattern: "stablediffussion",
+alias: ["sd", "imagine2"],
+react: "”„",
+desc: "Generate an image using AI.",
+category: "main",
+filename: __filename
+}, async (conn, mek, m, {
+from,
+quoted,
+body,
+isCmd,
+command,
+args,
+q,
+isGroup,
+sender,
+senderNumber,
+botNumber2,
+botNumber,
+pushname,
+isMe,
+isOwner,
+groupMetadata,
+groupName,
+participants,
+groupAdmins,
+isBotAdmins,
+isAdmins,
+reply
+}) => {
+try {
+if (!q) return reply("Please provide a prompt for the image.");
+
+// Indicate processing of user request
+await reply("*Subzero Diffussing Your image...*");
+
+// Generate and send requested image
+let data = await fetchJson(`https://api.davidcyriltech.my.id/diffusion?prompt=${q}`);
+const imageUrl = data.result;
+await conn.sendMessage(m.chat, { image: { url: imageUrl } });
+
+} catch (error) {
+console.error(error);
+reply(`An error occurred: ${error.message}`);
+}
+});
